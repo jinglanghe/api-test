42bf6d7c10a5fb635bf801beb4772ee9b4c1d3e8
hjl <jinglang.he@apulis.com>
2021-08-13 18:28:03 +0800

    add testcase

--- draft/draft.py
+++ draft/draft.py
@@ -6,30 +6,9 @@
 @DESC  : 
 """
 
-import requests
-import json
-import urllib
-url = "http://192.168.1.183/custom-user-dashboard-backend/group"
+from hamcrest import *
 
-headers = {"content-type": "application/json",'authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjMwMDAxLCJ1c2VyTmFtZSI6ImFkbWluIiwiZXhwIjoxNjI4OTEwNzY2LCJpYXQiOjE2Mjg3Mzc5NjZ9.o8AZMmXMZh95dlpHlR-FmvpqGhy3BgmpB6UFdQjqQa4'}
+list_a = [ 'f8e35b43', '0cef7c62', '是Ss12-_AS', '第十个手动if无偶诶你偶读未发文', '第九个时代峻峰危机期间欧文', '第八个发窘文件覅偶全文翻', '第七个清热贵而且荣女', '第七个水电费加豆腐囧文件', '第六个的空间哥今儿个接耳机', '第五个的附近噢我微积分琼文附近噢我', '第四个圣诞节卢浮宫科荣', '第三个电饭锅和如果', '第二个项目looklook', '创建一个项目试试看']
 
-payload = {
-    "name":"撒的发生",
-    "note":"撒旦法撒旦法",
-    "role":[1]
-}
-
-print(payload)
-r = requests.request("post", url=url, headers=headers, json=payload)
-
-print(r.url)
-print(r.status_code)
-print(r.request.method)
-print(r.encoding)
-# print(r.request.body)
-if r.request.body:
-    print(json.loads(r.request.body))
-else:
-    print(r.request.body)
-# print(r.content)
-# print(r.json())
+pro = 'Project_e4877286'
+assert_that(pro, is_in(list_a))
--- testcase/test_projects/test_projects.py
+++ testcase/test_projects/test_projects.py
@@ -15,7 +15,10 @@ from datetime import datetime
 from hamcrest import *
 from tools.run_log import RunLog
 import allure
+import time
+
 logger = RunLog()
+
 
 @allure.feature('项目管理测试用例')
 class TestProtects:
@@ -25,38 +28,84 @@ class TestProtects:
         pwd = os.path.dirname(__file__)
         self.read = ReadConfig()
         self.test_data_projects = self.read.read_config(os.path.join(pwd, '../../test_data/test_projects.yaml'))
-    '''
-    def test_get_scenes_list(self):
-        r = self.scenes.get_scenes_list()
-        assert r.get("code", "get failed") == 0
+        self.now_time = datetime.strftime(datetime.now(), '%Y-%m-%d  %H:%M:%S')
+        logger.info('执行项目管理测试用例')
 
-    def test_get_scene_labels(self):
-        r = self.scenes.get_scene_labels()
-        scene_labels = set(jsonpath(r, '$..data')[0])  # 系统中一般预置了["det_cls", "ocr", "seg"]的标签
-        assert set(self.test_data_scenes.get('data').get('scene_labels')) == set(scene_labels)
-    '''
-    # def test_create_pro_project(self):
-    #     payload: dict = self.test_data_projects.get('data').get('create_project')
-    #
-    #     # 当前时间，用于写在描述当中
-    #     now_time = str(datetime.now().strftime("%Y-%m-%d  %H:%M:%S"))
-    #     payload['name'] = f'Project_{str(uuid.uuid4())[:8]}'
-    #     payload['description'] = f'创建项目， {now_time}'
-    #     payload['type'] = 0
-    #     print(payload)
-    #     r = self.projects.create_project(payload)
-    #     # TODO
+        # 处理项目名称、描述、类型，得到payload
+        # eg:
+        # {'name':'Project_d9e1e2c5', 'description':'description d9e1e2c5,创建时间2021-08-13 15:50:41', 'type':'专家模式'}
+        uuid_front_8 = str(uuid.uuid4())[:8]
+        self.payload: dict = self.test_data_projects['data']['create_project']
+        type_need = self.payload['type_pro']
+        self.payload['name'] = f'Project_{uuid_front_8}'
+        self.payload['description'] = f'description {uuid_front_8},  创建时间{self.now_time}'
+        self.payload.pop('type_pro')
+        self.payload.pop('type_preset')
+        self.payload['type'] = type_need
+        # 处理项目名称、描述、类型，得到payload
 
-    @allure.story('项目列表接口，测试项目列表默认按创建时间倒序排返回')
-    def test_get_project_lists(self):
-        r = self.projects.get_project_lists()
-        r_jsonpath : list = jsonpath(r, '$..data..items..createdAt') # 获取所有项目的创建时间数组
+    @allure.story('项目列表接口，测试正常创建专家模式项目')
+    def test_create_project(self):
 
+        logger.info('测试创建专家模型项目')
+        r = self.projects.create_project(self.payload)  # 创建项目
+
+        r_list = self.projects.get_project_lists()  # 查询项目列表返回
+        r_list_json: list = jsonpath(r_list, '$..data..items..name')  # 项目列表中所有的项目名称
+        time.sleep(1)
         try:
-            assert_that(r_jsonpath, equal_to(sorted(r_jsonpath, reverse=True))) # 原数组是否和倒序排序后的数组相等？
+            # assert_that(r_list_json, has_item(payload['name']))
+            assert self.payload['name'] in r_list_json  # 判断创建的项目是否在列表中
         except Exception as e:
-            logger.error(f"断言失败{e}, 项目列表默认不是按倒序排返回。")
-            assert_that(r_jsonpath, equal_to(sorted(r_jsonpath, reverse=True)))
+            logger.error(f"断言失败{e}, 添加失败，创建的项目没有在列表中。")
+            # assert_that(r_list_json, has_item(payload['name']))
+            assert self.payload['name'] in r_list_json
         else:
-            logger.info("断言成功，项目列表默认按创建时间倒序排返回。")
+            logger.info("断言成功，项目添加成功。")
+
+        #  数据清理
+        project_id = jsonpath(r, '$.data.id')[0]
+        self.projects.delete_project(project_id)
+        logger.info(f"数据清理，删除项目，id {project_id}。")
+
+    @allure.story('项目列表接口，测试项目列表默认按创建时间【倒序】排返回')
+    def test_get_project_lists(self):
+        logger.info('测试项目列表接口')
+        r = self.projects.get_project_lists()  # 发送请求
+        if jsonpath(r, '$..data..items..createdAt'):
+            r_jsonpath: list = jsonpath(r, '$..data..items..createdAt')  # 获取所有项目的创建时间数组
+
+            try:
+                # assert_that(r_jsonpath, equal_to(sorted(r_jsonpath, reverse=True)))
+                assert r_jsonpath == sorted(r_jsonpath, reverse=True)  # 原数组是否和倒序排序后的数组相等？
+            except Exception as e:
+                logger.error(f"断言失败{e}, 项目列表默认不是按倒序排返回。")
+                assert r_jsonpath == sorted(r_jsonpath, reverse=True)
+            else:
+                logger.info("断言成功，项目列表默认按创建时间倒序排返回。")
+
+    @allure.story('项目列表接口，正常删除项目')
+    def test_delete_project(self):
+
+        r_create = self.projects.create_project(self.payload)  # 创建项目
+        project_id = jsonpath(r_create, '$.data.id')[0]
+        time.sleep(1)
+
+        self.projects.delete_project(project_id)  #  删除项目
+        time.sleep(1)
+
+        r_list = self.projects.get_project_lists()  # 查询项目列表返回
+        r_list_json: list = jsonpath(r_list, '$..data..items..name')  # 项目列表中所有的项目名称
+        try:
+            assert self.payload['name'] not in r_list_json  # 判断创建的项目是否在列表中
+        except Exception as e:
+            logger.error(f"断言失败{e}, 删除项目失败，项目还是出现在列表中。")
+            assert self.payload['name'] not in r_list_json
+        else:
+            logger.info("断言成功，删除项目成功。")
+
+    # @allure.story('项目列表接口，获取项目详情')
+    # def test_get_project(self):
+    # TODO
+
 
